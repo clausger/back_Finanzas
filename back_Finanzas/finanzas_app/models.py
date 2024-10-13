23a8e646d2c8e05170f9c24da4cdf81e21e638b1
FrancoArriola7 <rulox0203@gmail.com>
2024-10-13 20:14:57 -0300

    Borre fuente financiacion

--- back_Finanzas/finanzas_app/models.py
+++ back_Finanzas/finanzas_app/models.py
@@ -27,16 +27,6 @@ class Gasto(models.Model):
     def __str__(self):
         return f"{self.description} - {self.amount}"
 
-
-class FuenteFinanciacion(models.Model):
-    tipo = models.CharField(max_length=100)
-    monto = models.DecimalField(max_digits=10, decimal_places=2)
-    tasa_interes = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)  # Opcional
-    plazo = models.IntegerField(null=True, blank=True)  # Opcional (en meses o días)
-
-    def __str__(self):
-        return f"{self.tipo} - {self.monto}"
-
 class IngresoProyectado(models.Model):
     anio = models.IntegerField()
     monto = models.DecimalField(max_digits=10, decimal_places=2)
@@ -50,9 +40,8 @@ class Proyecto(models.Model):
     costo_total = models.DecimalField(max_digits=15,decimal_places=2)
     duracion = models.IntegerField(help_text="Duración en días")
     fecha_inicio = models.DateField()
-    fuentes_financiacion = models.ManyToManyField(FuenteFinanciacion, related_name='proyectos')
     ingresos_proyectados = models.ManyToManyField(IngresoProyectado, related_name='proyectos')
-    roi = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)  # Nueva columna para ROI
+    roi = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
 
 
     def __str__(self):
