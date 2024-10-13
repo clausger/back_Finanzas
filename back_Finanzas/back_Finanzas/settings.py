8c5291888829945c4500fb9aac7d02c42c490dbe
FrancoArriola7 <rulox0203@gmail.com>
2024-10-13 17:58:05 -0300

    Ajuste de settings

--- back_Finanzas/back_Finanzas/settings.py
+++ back_Finanzas/back_Finanzas/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True  # Permite todas las solicitudes
