ec7709ca1d80d635fa6b9d3035957f3700e71a0d
FrancoArriola7 <rulox0203@gmail.com>
2024-10-13 20:31:09 -0300

    Borre fuente financiacion

--- back_Finanzas/finanzas_app/serializers.py
+++ back_Finanzas/finanzas_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Ingreso, Gasto, FuenteFinanciacion, IngresoProyectado, Proyecto
+from .models import Ingreso, Gasto, IngresoProyectado, Proyecto
 
 class IngresoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,18 +11,12 @@ class GastoSerializer(serializers.ModelSerializer):
         model = Gasto
         fields = '__all__'
 
-class FuenteFinanciacionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FuenteFinanciacion
-        fields = ['tipo', 'monto', 'tasa_interes', 'plazo']
-
 class IngresoProyectadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = IngresoProyectado
         fields = ['anio', 'monto']
 
 class ProyectoSerializer(serializers.ModelSerializer):
-    fuentes_financiacion = FuenteFinanciacionSerializer(many=True)
     ingresos_proyectados = IngresoProyectadoSerializer(many=True)
 
     class Meta:
@@ -33,9 +27,6 @@ class ProyectoSerializer(serializers.ModelSerializer):
         fuentes_data = validated_data.pop('fuentes_financiacion')
         ingresos_data = validated_data.pop('ingresos_proyectados')
         proyecto = Proyecto.objects.create(**validated_data)
-
-        for fuente_data in fuentes_data:
-            FuenteFinanciacion.objects.create(proyecto=proyecto, **fuente_data)
 
         for ingreso_data in ingresos_data:
             IngresoProyectado.objects.create(proyecto=proyecto, **ingreso_data)
