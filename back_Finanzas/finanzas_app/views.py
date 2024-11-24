c820c4955b9e1c3b2f6506950b0dbc79dd4607e2
Pedro <pedroschon3005@gmail.com>
2024-11-24 00:33:32 -0300

    update on dashboard

--- back_Finanzas/finanzas_app/views.py
+++ back_Finanzas/finanzas_app/views.py
@@ -18,31 +18,32 @@ class ProyectoViewSet(viewsets.ModelViewSet):
     queryset = Proyecto.objects.all()
     serializer_class = ProyectoSerializer
 
+
 class ResumenFinancieroCompletoView(APIView):
     """
     API personalizada que devuelve:
-    1. La suma de 'Costo Total' de todos los proyectos.
-    2. La suma de todos los ingresos con 'tipo_ingreso == Recurrente'.
-    3. La suma de todos los gastos con 'type == Recurrente'.
-    """
-    
+
+La suma de 'Costo Total' de todos los proyectos.
+La suma de todos los ingresos.
+La suma de todos los gastos.
+"""
+
+
     def get(self, request, format=None):
-        # 1. Suma del costo total en todos los proyectos
         total_costo_proyectos = Proyecto.objects.aggregate(Sum('costo_total'))['costo_total__sum'] or 0
-        
-        # 2. Suma de todos los ingresos donde 'tipo_ingreso' es Recurrente
-        total_ingresos_recurrentes = Ingreso.objects.filter(tipo_ingreso="Recurrente").aggregate(Sum('amount'))['amount__sum'] or 0
-        
-        # 3. Suma de todos los gastos donde 'type' es Recurrente
-        total_gastos_recurrentes = Gasto.objects.filter(type="Recurrente").aggregate(Sum('amount'))['amount__sum'] or 0
-        
-        balance = total_ingresos_recurrentes - total_gastos_recurrentes
+
+        total_ingresos = Ingreso.objects.aggregate(Sum('amount'))['amountsum'] or 0
+
+        total_gastos = Gasto.objects.aggregate(Sum('amount'))['amountsum'] or 0
+
+        balance = total_ingresos - total_gastos
+
         return Response({
             'total_costo_proyectos': total_costo_proyectos,
-            'total_ingresos_recurrentes': total_ingresos_recurrentes,
-            'total_gastos_recurrentes': total_gastos_recurrentes,
-            'total_balance':balance,
+            'total_ingresos_recurrentes': total_ingresos,
+            'total_gastos_recurrentes': total_gastos,
+            'total_balance': balance,
         })
-    
+
 def ping(request):
     return JsonResponse({'message': 'Pong'})
